4cfbcc6131c09bcd6bd02629e525ee146e953a0e
Olusegun Olayinka <olayinkasegunsolo@gmail.com>
2026-03-13 20:19:37 +0100

    add user validation

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,34 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { createUser } from "./users.service";
+import { createUserSchema } from "./users.schema";
 
 export async function createUserHandler(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  const { email, password } = request.body as any;
+  try {
+    const parsed = createUserSchema.parse(request.body);
 
-  const user = await createUser(request.server, email, password);
+    const { email, password } = parsed;
 
-  return reply.code(201).send(user);
+    const user = await createUser(request.server, email, password);
+
+    return reply.code(201).send(user);
+  } catch (err: any) {
+    if (err.code === "23505") {
+      return reply
+        .code(409)
+        .send({ error: "Email already exists", details: err.detail });
+    }
+
+    if (err?.name === "ZodError") {
+      return reply
+        .code(400)
+        .send({ error: "Invalid request", issues: err.errors });
+    }
+
+    // fallback
+    request.log.error(err);
+    return reply.code(500).send({ error: "Internal server error" });
+  }
 }
