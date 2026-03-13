9250c3d1ecf0189e9869fc2bf0fa1b93a5e5c3e8
Olusegun Olayinka <olayinkasegunsolo@gmail.com>
2026-03-13 18:21:08 +0100

    migrate users table

---
+++ src/db/migrations/20260313164709_create_users_table.ts
@@ -0,0 +1,14 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("users", (table) => {
+    table.uuid("id").primary();
+    table.string("email").notNullable().unique();
+    table.string("password_hash").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("users");
+}
