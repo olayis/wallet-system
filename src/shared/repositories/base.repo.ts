8faeba96cd6bea8561fef47478ccd4fa894a5714
Olusegun Olayinka <olayinkasegunsolo@gmail.com>
2026-04-12 23:09:21 +0100

    feat: add base repo

---
+++ src/shared/repositories/base.repo.ts
@@ -0,0 +1,41 @@
+import { Model, QueryBuilder, Transaction } from "objection";
+import { ObjectLiteral } from "../../types/object-literal.type";
+
+export class BaseRepository<M extends Model> {
+  private model: typeof Model | any;
+
+  constructor(model: M | any) {
+    this.model = model;
+  }
+
+  protected query(trx?: Transaction) {
+    return this.model.query(trx);
+  }
+
+  async findById(id: string, trx?: Transaction): Promise<M> {
+    return await this.query(trx).findById(id);
+  }
+
+  async findOne(
+    criteria: ObjectLiteral,
+    trx?: Transaction,
+  ): Promise<M | undefined> {
+    return await this.query(trx).findOne(criteria);
+  }
+
+  async findAll(trx?: Transaction): Promise<M[]> {
+    return await this.query(trx).orderBy("createdAt", "desc");
+  }
+
+  async save(data: any, trx?: Transaction): Promise<M> {
+    return await this.query(trx).insert(data).returning("*");
+  }
+
+  async updateById(id: string, data: any, trx?: Transaction): Promise<M> {
+    return await this.query(trx).patchAndFetchById(id, data);
+  }
+
+  async deleteById(id: string, trx?: Transaction): Promise<number> {
+    return await this.query(trx).deleteById(id);
+  }
+}
